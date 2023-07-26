a1aef8140b99c94099b06653bf18b36b54339979
manokrish2000 <manokrish104525@gmail.com>
2023-07-26 16:30:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv')
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/user", userRoutes);
+app.get('/', function (req, res) {
+  res.send('Hello World1');
+})
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
